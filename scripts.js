04941fc53a0e389e59452051893bafb8ba27c5a0
CoinzPlayz <146862579+CoinzPlayz@users.noreply.github.com>
2023-11-05 10:01:42 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-const
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
