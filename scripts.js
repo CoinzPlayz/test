0f025edca269075bf21fda0647ae35d95219dc36
CoinzPlayz <146862579+CoinzPlayz@users.noreply.github.com>
2023-11-05 22:06:53 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,16 +10,3 @@ function dropDown() {
 function animatedMenu(x) {
   x.classList.toggle("change");
 }
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
